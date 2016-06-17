de66c77fb3dc5e54fa97b906448d67bfa4cb0eb8
dsagayaraj <daniel.sagayarajpeter@adp.com>
2016-06-17 14:16:29 +0530

    electron args

--- html/app.js
+++ html/app.js
@@ -1,3 +1,3 @@
 var remote = require('electron').remote,
 arguments = remote.getGlobal('sharedObject').prop1;
-document.getElementById("user_name").innerHTML="Daniel"
+document.getElementById("user_name").innerHTML=arguments[0]
